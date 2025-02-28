f139903388d371cfcae5f6fa3ab7832829a34721
shalini-thilakan <shalini.thilakan@gmail.com>
2025-02-28 07:14:58 +0530

    SCRUM-1 updated gpt model 1

--- validate_automation_scripts.py
+++ validate_automation_scripts.py
@@ -6,7 +6,7 @@ ACCEPTANCE_CRITERIA = sys.argv[1]
 FEATURE_FILES = sys.argv[2].split("\n")
 STEP_DEF_FILES = sys.argv[3].split("\n")
 
-GPT_MODEL = "gpt-4o"
+GPT_MODEL = "gpt-3.5-turbo"
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
