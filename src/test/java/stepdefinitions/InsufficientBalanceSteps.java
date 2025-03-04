d03143617d7e3485a6ee7e1ec45bed5570f4f65d
shalini-thilakan <shalini.thilakan@gmail.com>
2025-03-05 02:08:09 +0530

    SCRUM-5 Added missing scenario

--- src/test/java/stepdefinitions/InsufficientBalanceSteps.java
+++ src/test/java/stepdefinitions/InsufficientBalanceSteps.java
@@ -1,6 +1,9 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.*;
+
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,11 +14,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class InsufficientBalanceSteps {
     WebDriver driver;
 
+    private boolean hasSufficientBalance;
+    private boolean transactionApproved; 
+
     @Given("a customer with a debit card and insufficient balance")
     public void customer_with_insufficient_balance() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://banking-portal.com/login");
+
+        hasSufficientBalance = false; // Assuming the customer has insufficient balance
+
     }
 
     @When("they attempt an online transaction")
@@ -23,6 +32,12 @@ public class InsufficientBalanceSteps {
         driver.findElement(By.id("paymentPage")).click();
         driver.findElement(By.id("amount")).sendKeys("5000");
         driver.findElement(By.id("payButton")).click();
+
+        if (hasSufficientBalance) {
+            transactionApproved = true; // Simulate transaction approval
+        } else {
+            transactionApproved = false;
+        }
     }
 
     @Then("the transaction should be declined")
@@ -30,5 +45,15 @@ public class InsufficientBalanceSteps {
         WebElement errorMessage = driver.findElement(By.id("insufficientFundsError"));
         Assert.assertTrue(errorMessage.isDisplayed());
         driver.quit();
+    }
+
+    @Given("a customer with a debit card and sufficient balance")
+    public void a_customer_with_a_debit_card_and_sufficient_balance() {
+        hasSufficientBalance = true; // Assuming the customer has enough balance
+    }
+
+    @Then("the transaction should be accepted")
+    public void the_transaction_should_be_accepted() {
+        assertTrue("Transaction was not approved despite sufficient balance", transactionApproved);
     }
 }
--- src/test/resources/features/InsufficientBalance.feature
+++ src/test/resources/features/InsufficientBalance.feature
@@ -2,3 +2,8 @@ Scenario: Customer tries to make an online payment with insufficient balance
   Given a customer with a debit card and insufficient balance
   When they attempt an online transaction
   Then the transaction should be declined
+
+Scenario: Customer tries to make an online payment with sufficient balance
+  Given a customer with a debit card and sufficient balance
+  When they attempt an online transaction
+  Then the transaction should be accepted
